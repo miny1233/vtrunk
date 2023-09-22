61813749261249db0c1259e19128926832a2b0ec
miny1233 <1596784863@qq.com>
2023-09-22 17:32:48 +0800

    负载均衡造好了

--- include/load_balance.h
+++ include/load_balance.h
@@ -12,26 +12,35 @@
 class load_balance {
 private:
     std::set<tunnel> socket_fd_list;
-    std::set<tunnel>::iterator s_tunnel;
+    std::set<tunnel>::iterator s_tunnel,r_tunnel;
 public:
     load_balance():socket_fd_list(),s_tunnel(socket_fd_list.begin()){};
     void add_tunnel(tunnel fd)
     {
         s_tunnel = socket_fd_list.insert(fd).first;
+        r_tunnel = s_tunnel;
     }
     void del_tunnel(tunnel fd)
     {
         socket_fd_list.erase(fd);
+        s_tunnel = socket_fd_list.begin();
+        r_tunnel = s_tunnel;
     }
     ssize_t send_next(const void* buf,size_t len)
     {
         if (s_tunnel == socket_fd_list.end())s_tunnel = socket_fd_list.begin();
-        return sendto(s_tunnel->fd,buf,len,MSG_DONTWAIT,&s_tunnel->objective,sizeof(s_tunnel->objective));
+        return sendto(s_tunnel++->fd,buf,len,MSG_DONTWAIT,&s_tunnel->objective,sizeof(s_tunnel->objective));
     }
 
     ssize_t recv_next(void* buf,size_t buf_size)
     {
-
+        reget:
+        if(r_tunnel == socket_fd_list.end())r_tunnel = socket_fd_list.begin();
+        sockaddr none{};
+        socklen_t none_len;
+        ssize_t ret = recvfrom(r_tunnel++->fd,buf,buf_size,MSG_DONTWAIT,&none,&none_len);
+        if(ret == -1)goto reget;
+        return ret;
     }
 
 };
